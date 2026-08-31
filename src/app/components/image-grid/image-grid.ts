ce97e25af0ec0aa298fafaa157512f5a19d6b86d
alessia <alessia.zacco12@gmail.com>
2026-08-31 17:27:44 +0200

    Add image grid component logic

---
+++ src/app/components/image-grid/image-grid.ts
@@ -0,0 +1,11 @@
+// Questo file definisce il componente Image Grid. Il layout della galleria viene gestito tramite HTML e CSS Grid.
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-grid',
+  imports: [],
+  templateUrl: './image-grid.html',
+  styleUrl: './image-grid.css',
+})
+export class ImageGrid {}
